fedcf3da6c4781759c758b0d238fa11785aa994a
AudriusLunys <lunysaudrius@inbox.lt>
2021-11-17 14:18:00 +0200

    device repo

--- registration/src/main/java/lt/codeacademy/registration/repository/DeviceRepository.java
+++ registration/src/main/java/lt/codeacademy/registration/repository/DeviceRepository.java
@@ -1,16 +1,13 @@
 package lt.codeacademy.registration.repository;
 
-import lt.codeacademy.registration.model.Customer;
 import lt.codeacademy.registration.model.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
 
     @Override
-    Optional<Customer> findById(UUID uuid);
-
+    Optional<Device> findById(UUID uuid);
 }
