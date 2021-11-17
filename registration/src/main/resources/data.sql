8e907d1839b121e115390cb31fb6e1cf87cb0b1f
AudriusLunys <lunysaudrius@inbox.lt>
2021-11-17 15:30:28 +0200

    data.sql updated with insert customers

--- registration/src/main/resources/data.sql
+++ registration/src/main/resources/data.sql
@@ -5,3 +5,10 @@ INSERT INTO DEVICES (uuid, manufacturer, model, serial_Number, failure_Descripti
  ('9b4018ac-bac2-4afe-8ac6-055612ba9d43', 'Samsung', 'Galaxy S10', 'ASD551AS', 'Broken Screen'),
  ('9b4018ac-bac2-4afe-8ac6-055612ba9d44', 'ASUS', 'X14ZFR', 'A4S551AS', 'Spill damage'),
  ('9b4018ac-bac2-4afe-8ac6-055612ba9d49', 'ASUS', 'XWZ', 'ASD551AS', 'Broken hinges');
+
+INSERT INTO CUSTOMERS (uuid, first_name, last_name, email, tel_number) VALUES
+('114018ac-bac2-4afe-8ac6-055612ba9d40', 'Audrius', 'Lunys', 'vienas@du.lt', '+3705465454'),
+('124018ac-bac2-4afe-8ac6-055612ba9d40', 'Dalia', 'Daliene', 'as@du.lt', '+370569844'),
+('134018ac-bac2-4afe-8ac6-055612ba9d40', 'Inga', 'Ingaite', 'vd@gmail.com', '+3705865454'),
+('144018ac-bac2-4afe-8ac6-055612ba9d40', 'Stasys', 'Stasiulis', 'trys@du.lt', '+370415454'),
+('154018ac-bac2-4afe-8ac6-055612ba9d40', 'Jonas', 'Jonka', 'vis@yahoo.com', '+3705488454');
